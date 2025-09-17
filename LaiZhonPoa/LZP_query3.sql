dd4092ae765172b284c2cfdf29ea1677d054c25a
NotABot <135363764+BryanTheLai@users.noreply.github.com>
2025-09-17 21:52:44 +0800

    Newest aligned

--- LaiZhonPoa/LZP_query3.sql
+++ LaiZhonPoa/LZP_query3.sql
@@ -1,4 +1,3 @@
--- Report 3: Hotel Upselling Effectiveness
 
 DEFINE LATEST_YEAR = 2025
 
@@ -7,11 +6,10 @@ SET LINESIZE 180
 SET VERIFY OFF
 SET FEEDBACK OFF
 
--- Set the report titles, using the variable we just defined.
-TTITLE CENTER 'Hotel Analytics Inc.' SKIP 1 CENTER 'Hotel Upselling Effectiveness by Peer Group' SKIP 1 CENTER '(Analysis for the Year &LATEST_YEAR)' SKIP 2
-BTITLE CENTER 'Page ' FORMAT 999 SQL.PNO SKIP 1 CENTER 'Report Generated on: ' _DATE
+TTITLE CENTER 'Hotel Analytics Inc.' SKIP 1 CENTER 'Hotel Upselling Effectiveness by Peer Group' SKIP 1 -
+CENTER '(Analysis for the Year &LATEST_YEAR)' SKIP 1 -
+LEFT 'Page ' FORMAT 999 SQL.PNO COL 112 'Report Generated on: ' _DATE SKIP 2
 
--- Define the column formats and headings for the report body.
 COLUMN "Peer Rank"            FORMAT A20
 COLUMN City                   FORMAT A25
 COLUMN Country                FORMAT A40
@@ -19,7 +17,6 @@ COLUMN "Upselling Ratio"      FORMAT A18 HEADING 'Upselling Ratio'
 COLUMN "Avg Room Rate"        FORMAT $99,990.00
 COLUMN "Extra Spending/Night" FORMAT $99,990.00
 
--- Main Query
 WITH
   HotelRoomMetrics AS (
     SELECT
@@ -28,7 +25,6 @@ WITH
       SUM(fbr.CalculatedBookingAmount) / SUM(fbr.DurationDays) AS AvgRoomRate
     FROM FactBookingRoom fbr
     JOIN DimDate dd ON fbr.DateKey = dd.DateKey
-    -- Use the defined variable
     WHERE dd.Year = &LATEST_YEAR AND fbr.DurationDays > 0
     GROUP BY fbr.HotelKey
   ),
@@ -36,7 +32,6 @@ WITH
     SELECT ffb.HotelKey, SUM(ffb.FacilityTotalAmount) AS TotalFacilityRevenue
     FROM FactFacilityBooking ffb
     JOIN DimDate dd ON ffb.DateKey = dd.DateKey
-    -- Use the defined variable
     WHERE dd.Year = &LATEST_YEAR
     GROUP BY ffb.HotelKey
   ),
@@ -58,7 +53,7 @@ SELECT
   COUNT(*) OVER (PARTITION BY hp.Rating) AS "Peer Rank",
   hp.City,
   hp.Country,
-  TO_CHAR((hp.ExtraSpendingPerNight / hp.AvgRoomRate) * 100, 'FM990.0') || '%' AS "Upselling Ratio",
+  LPAD(TO_CHAR((hp.ExtraSpendingPerNight / hp.AvgRoomRate) * 100, 'FM990.0') || '%', 18) AS "Upselling Ratio",
   hp.AvgRoomRate AS "Avg Room Rate",
   hp.ExtraSpendingPerNight AS "Extra Spending/Night"
 FROM HotelPerformance hp
@@ -67,5 +62,6 @@ ORDER BY hp.Rating DESC, (hp.ExtraSpendingPerNight / hp.AvgRoomRate) DESC;
 
 CLEAR COLUMNS
 TTITLE OFF
-BTITLE OFF
 UNDEFINE LATEST_YEAR
+
+
